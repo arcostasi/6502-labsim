8fbee489e1671ed611b1824a04128a15dc245ec7
Anderson Costa <arcostasi@gmail.com>
2026-01-11 21:30:00 -0300

    feat(visualizers): add Visualizers aggregator panel

---
+++ components/Visualizers.tsx
@@ -0,0 +1,10 @@
+
+// Re-export all visualizer components from their individual files
+export { CpuBreakout } from './visualizers/CpuBreakout';
+export { ViaBreakout } from './visualizers/ViaBreakout';
+export { RomBreakout } from './visualizers/RomBreakout';
+export { TiaBreakout } from './visualizers/TiaBreakout';
+export { ClockBreakout } from './visualizers/ClockBreakout';
+export { AciaBreakout } from './visualizers/AciaBreakout';
+export { TiaAudioKeyboard } from './visualizers/TiaAudioKeyboard';
+export { Crtc6845Board } from './visualizers/Crtc6845Board';
